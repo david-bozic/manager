36ad4c95536586325ce4afc97b100bcedb4ac999
David <david.bozic@aaronprojects.de>
2017-10-23 22:06:06 +0200

    setup done
    reusable components, firebase, linter. redux

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import reducers from './reducers';
+import firebase from 'firebase';
 
 class App extends Component {
+componenWillMount() {
+const config = {
+  apiKey: 'AIzaSyDYYWs7xN9GHTzzabOqR-j7z8sBZ5sYuoY',
+  authDomain: 'manager-70877.firebaseapp.com',
+  databaseURL: 'https://manager-70877.firebaseio.com',
+  projectId: 'manager-70877',
+  storageBucket: 'manager-70877.appspot.com',
+  messagingSenderId: '705570908030'
+};
+firebase.initializeApp(config);
+  }
+
   render() {
     return (
-      <Provider store={createStore()}>
-        <Text>
-          Hello!
-        </Text>
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>
+            Hello!
+          </Text>
+      </View>
       </Provider>
     );
   }
