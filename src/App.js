d5ae6f7076bbb418dd9c2fe28bb52f154ed89eb9
David <david.bozic@aaronprojects.de>
2017-11-03 18:12:12 +0100

    firebase error corrected

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import firebase from 'firebase';
 import ReduxThunk from 'redux-thunk';
+import firebase from 'firebase';
 import reducers from './reducers';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
-componenWillMount() {
-firebase.initializeApp({
-  apiKey: 'AIzaSyDYYWs7xN9GHTzzabOqR-j7z8sBZ5sYuoY',
-  authDomain: 'manager-70877.firebaseapp.com',
-  databaseURL: 'https://manager-70877.firebaseio.com',
-  projectId: 'manager-70877',
-  storageBucket: 'manager-70877.appspot.com',
-  messagingSenderId: '705570908030'
-  });
+componentWillMount() {
+  const config = {
+    apiKey: 'AIzaSyDYYWs7xN9GHTzzabOqR-j7z8sBZ5sYuoY',
+    authDomain: 'manager-70877.firebaseapp.com',
+    databaseURL: 'https://manager-70877.firebaseio.com',
+    projectId: 'manager-70877',
+    storageBucket: 'manager-70877.appspot.com',
+    messagingSenderId: '705570908030'
+  };
+  firebase.initializeApp(config);
 }
 
   render() {
