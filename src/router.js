02952e23d83374b2eae9f35a8c238b3d542f8388
David <david.bozic@aaronprojects.de>
2017-11-05 17:41:14 +0100

    EmployeeCreate, EmployeeList, EmployeeFormReducers added
    We made the first successful Push request

--- src/router.js
+++ src/router.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
+
 
 const RouterComponent = () => {
   return (
     <Router>
-      <Scene key="root">
-        <Scene key="login" component={LoginForm} title="Login" />
+      <Scene key="root" hideNavBar>
+        <Scene key="auth">
+          <Scene key="login" component={LoginForm} title="Please Login" />
+        </Scene>
+
+        <Scene key="main">
+          <Scene
+            onRight={() => Actions.EmployeeCreate()}
+            rightTitle="Add"
+            key="EmployeeList"
+            component={EmployeeList}
+            title="Employees"
+            initial
+          />
+          <Scene
+            key="EmployeeCreate"
+            component={EmployeeCreate}
+            title="Create Employee"
+          />
+        </Scene>
       </Scene>
     </Router>
   );
