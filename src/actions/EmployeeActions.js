c12812c74f6e73643eec2f0f963b4c0f4a9d2b00
David <david.bozic@aaronprojects.de>
2017-11-09 22:45:06 +0100

    done for now :)

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -3,7 +3,8 @@ import { Actions } from 'react-native-router-flux';
 import {
   EMPLOYEE_UPDATE,
   EMPLOYEE_CREATE,
-  EMPLOYEES_FETCH_SUCCESS
+  EMPLOYEES_FETCH_SUCCESS,
+  EMPLOYE_SAVE_SUCCESS
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -32,6 +33,28 @@ export const employeesFetch = () => {
     firebase.database().ref(`/users/${currentUser.uid}/employees`)
     .on('value', snapshot => {
       dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() });
+    });
+  };
+};
+
+export const employeSave = ({ name, phone, shift, uid }) => {
+  const { currentUser } = firebase.auth();
+  return (dispatch) => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
+    .set({ name, phone, shift })
+    .then(() => {
+      dispatch({ type: EMPLOYE_SAVE_SUCCESS });
+      Actions.main({ type: 'reset' });
+     });
+  };
+};
+export const employeeDelete = ({ uid }) => {
+  const { currentUser } = firebase.auth();
+  return () => {
+  firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
+    .remove()
+    .then(() => {
+      Actions.employeeList({ type: 'reset' });
     });
   };
 };
--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
 import EmployeeCreate from './components/EmployeeCreate';
-
+import EmployeeEdit from './components/EmployeeEdit';
 
 const RouterComponent = () => {
   return (
@@ -26,6 +26,11 @@ const RouterComponent = () => {
             key="EmployeeCreate"
             component={EmployeeCreate}
             title="Create Employee"
+          />
+          <Scene
+            key="EmployeeEdit"
+            component={EmployeeEdit}
+            title="Edit Employee"
           />
         </Scene>
       </Scene>
